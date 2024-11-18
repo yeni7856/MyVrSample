26e7ade201bb5eca271e204a3e02ae0e04a4e60d
yeni7856 <86287877+yeni7856@users.noreply.github.com>
2024-11-18 18:22:26 +0900

    1. XR UI position Setting

--- Assets/Scripts/GameMenuUI.cs
+++ Assets/Scripts/GameMenuUI.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
 
 namespace MyVrSample
 {
+    /// <summary>
+    /// 게임중 메뉴 UI 관리하는 클래스
+    /// </summary>
     public class GameMenuUI : MonoBehaviour
     {
         #region Variables
         public GameObject gameMenuUI;
         public InputActionProperty showGameMenu;
+
+        public Transform head;
+        [SerializeField] private float distance = 1.5f;
+
+        //Drop UI
+        public SnapTurnProvider snapTurn;
+        public ContinuousTurnProvider continuousTurn;
         #endregion
 
         private void Update()
@@ -21,6 +32,38 @@ namespace MyVrSample
         void Toggle()
         {
             gameMenuUI.SetActive(!gameMenuUI.activeSelf);
+
+            //show 설정
+            if (gameMenuUI.activeSelf)
+            {
+                gameMenuUI.transform.position = head.position + new Vector3(head.forward.x, 0f, head.forward.z).normalized * distance;
+                gameMenuUI.transform.LookAt(new Vector3(head.position.x, gameMenuUI.transform.position.y, head.position.z));
+                gameMenuUI.transform.forward *= -1;
+            }
+
+        }
+
+        //drop 메뉴 선택
+        public void SetTurnTypeFromIndex(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    snapTurn.enabled = false;
+                    continuousTurn.enabled = true;
+                    break;
+                case 1:
+                    snapTurn.enabled = true;
+                    continuousTurn.enabled = false;
+                    break;
+            }
+        }
+
+        //Quit 버튼
+        public void Quit()
+        {
+            Debug.Log("Quit Game");
+            Application.Quit();
         }
     }
 }
